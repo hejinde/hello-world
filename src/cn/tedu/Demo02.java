819d93c53eb101489792df9a565ff5fe623499a8
hjd <ithejinde@163.com>
2019-08-21 16:59:13 +0800

    create Demo02

--- src/cn/tedu/Demo02.java
+++ src/cn/tedu/Demo02.java
@@ -1,6 +1,6 @@
 package cn.tedu;
 
-public class Demo01 {
+public class Demo02 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello world1");
