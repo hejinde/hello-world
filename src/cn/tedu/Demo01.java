db04294396de9a33028483e5c6ef2e9ec17d0785
hjd <ithejinde@163.com>
2019-08-21 10:58:27 +0800

    demo01 modify aaa-->fff

--- src/cn/tedu/Demo01.java
+++ src/cn/tedu/Demo01.java
@@ -2,7 +2,7 @@ package cn.tedu;
 
 public class Demo01 {
 	public static void main(String[] args) {
-		System.out.println("aaa");
+		System.out.println("fff");
 		System.out.println("ddd");
 		System.out.println("eee");
 		System.out.println("fff");
