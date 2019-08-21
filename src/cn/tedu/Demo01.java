1234df5fd1b67e5290d1c79cf3322cfeb9be2887
hjd <ithejinde@163.com>
2019-08-21 10:49:46 +0800

    demo01 add eee/fff

--- src/cn/tedu/Demo01.java
+++ src/cn/tedu/Demo01.java
@@ -3,6 +3,8 @@ package cn.tedu;
 public class Demo01 {
 	public static void main(String[] args) {
 		System.out.println("aaa");
-		System.out.println("ccc");
+		System.out.println("ddd");
+		System.out.println("eee");
+		System.out.println("fff");
 	}
 }
